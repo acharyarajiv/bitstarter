1e28317e17f56964660c6e960e533d8311847978
Rajeeva <acharya.rajiv@gmail.com>
2014-06-09 12:37:15 +0530

    test

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+var htmlfile = "index.html";
+
+app.get('/', function(request, response) {
+    var html = fs.readFileSync(htmlfile).toString();
+response.send(html);
+})
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
